d41821873a22c828cf9e6ea9be4d39db7daf0b96
kukinpower <thekukinpower@gmail.com>
2021-06-01 22:32:41 +0300

    done

--- src/main/java/mts/teta/resizer/imageprocessor/ImageProcessor.java
+++ src/main/java/mts/teta/resizer/imageprocessor/ImageProcessor.java
@@ -1,86 +1,18 @@
 package mts.teta.resizer.imageprocessor;
 
-import marvin.image.MarvinImage;
-import marvin.io.MarvinImageIO;
-import marvinplugins.MarvinPluginCollection;
-import mts.teta.resizer.ResizerApp;
-import mts.teta.resizer.exception.BadAttributesException;
-import net.coobird.thumbnailator.Thumbnails;
-
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import mts.teta.resizer.ResizerApp;
+import mts.teta.resizer.exception.BadAttributesException;
 
 public class ImageProcessor implements ImageCheckable {
-  private Thumbnails.Builder<BufferedImage> thumbnailsBuilder;
-  private MarvinImage marvinImage;
 
-  public void processImage (BufferedImage image, ResizerApp app) throws BadAttributesException, IOException {
-
-    if (!isValidArgsInput(app)) {
+  public void processImage(BufferedImage image, ResizerApp resizerApp)
+      throws BadAttributesException, IOException {
+    if (!isValidArgsInput(resizerApp)) {
       throw new BadAttributesException("Please check params!");
     }
 
+    new ImageContainer(image, resizerApp).changeQuality().resize().blur().crop().callWrite();
   }
 }
-
-
-//public class ImageProcessor implements ImageCheckable {
-//  private Thumbnails.Builder<BufferedImage> thumbnailsBuilder;
-//  private MarvinImage marvinImage;
-//
-//
-//  public void processImage (BufferedImage image, ResizerApp app) throws BadAttributesException, IOException {
-//
-//  private void processWithMarvinImage(BufferedImage bufferedImage, ResizerApp resizerApp)
-//      throws BadAttributesException {
-//    MarvinImage marvinImage = null;
-//
-//    if (resizerApp.getBlurRadius() != null) {
-//      marvinImage = new BlurTool().execute(bufferedImage, resizerApp);
-//    } else if (resizerApp.getCropWidth() != null) {
-//      marvinImage = new CropTool().execute(bufferedImage, resizerApp);
-//    }
-//
-//    if (marvinImage != null) {
-//      MarvinImageIO.saveImage(marvinImage, resizerApp.getOutputFile().getAbsolutePath());
-//    }
-//    throw new BadAttributesException("Please check params!");
-//  }
-//
-//  private void processWithThumbnailsBuilder(BufferedImage bufferedImage, ResizerApp resizerApp)
-//      throws IOException, BadAttributesException {
-//    Thumbnails.Builder<BufferedImage> thumbnailsBuilder = null;
-//
-//    if (resizerApp.getQuality() != null) {
-//      thumbnailsBuilder = new QualityTool().execute(bufferedImage, resizerApp);
-//    } else if (resizerApp.getResizeWidth() != null) {
-//      thumbnailsBuilder = new ResizeTool().execute(bufferedImage, resizerApp);
-//    } else if (resizerApp.getOutputFormat() != null) {
-//      thumbnailsBuilder = new FormatTool().execute(bufferedImage, resizerApp);
-//      if (thumbnailsBuilder == null) {
-//        throw new BadAttributesException("Please check params!");
-//      }
-//      return;
-//    }
-//    if (thumbnailsBuilder != null) {
-//      thumbnailsBuilder.outputFormat(resizerApp.getOutputFormat()).toFile(resizerApp.getOutputFile());
-//    }
-//    throw new BadAttributesException("Please check params!");
-//  }
-//
-//  public void processImage(BufferedImage bufferedImage, ResizerApp resizerApp)
-//      throws BadAttributesException, IOException {
-//
-//    // check arguments
-//    if (!isValidArgsInput(resizerApp)) {
-//      throw new BadAttributesException("Please check params!");
-//    }
-//
-//    if (resizerApp.getBlurRadius() != null || resizerApp.getCropWidth() != null) {
-//      processWithMarvinImage(bufferedImage, resizerApp);
-//    } else if (resizerApp.getQuality() != null || resizerApp.getResizeWidth() != null || resizerApp.getOutputFormat() != null) {
-//      processWithThumbnailsBuilder(bufferedImage, resizerApp);
-//    }
-//  }
-
-//}
--- src/main/java/mts/teta/resizer/tool/ImageTool.java
+++
@@ -1,9 +0,0 @@
-package mts.teta.resizer.tool;
-
-import java.awt.image.BufferedImage;
-import mts.teta.resizer.ResizerApp;
-
-public interface ImageTool<T> {
-
-  public T execute(BufferedImage bufferedImage, ResizerApp resizerApp);
-}
